b35c8a0f3af42afc1e2572f651d65bb48fbb77eb
Dracsis Cella De Cavalli Almeida <dcella-d@c1r4s4.42lisboa.com>
2023-06-02 16:57:20 +0100

    trying to solve add_split

--- libft/add_split.c
+++ libft/add_split.c
@@ -6,7 +6,7 @@
 /*   By: dcella-d <dcella-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 06:48:55 by dcella-d          #+#    #+#             */
-/*   Updated: 2023/06/02 16:29:14 by dcella-d         ###   ########.fr       */
+/*   Updated: 2023/06/02 16:56:46 by dcella-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ char	**add_split(char **split, char *new)
 	int		len;
 	char	**new_split;
 
-	if (split[0] == NULL || split == NULL || !(freedom("s", split)))
-		return (ft_split(new, 2));
+	if (split[0] == NULL || split == NULL)
+	{
+		new_split = ft_split(new, 2);
+		freedom("sa", split, new);
+		return (new_split);
+	}
 	new_split = (char **) ft_calloc(sizeof(char *), (word_count(split) + 2));
 	if (!new_split)
 		return (0);
@@ -27,8 +31,6 @@ char	**add_split(char **split, char *new)
 	while (split[++i])
 		new_split[i] = ft_strdup(split[i]);
 	new_split[i] = ft_strdup(new);
-	if (!new_split[i])
-		return (0);
 	new_split[++i] = NULL;
 	split = freedom("s", split);
 	return (new_split);
